6a484d340dc1bb25ff5783ae69e192a42f01a547
RACK-2026 <2796690649@qq.com>
2026-08-28 17:21:36 +0800

    Add server\socket.js

---
+++ server\socket.js
@@ -0,0 +1,20 @@
+// ===================== Socket.IO 事件处理 =====================
+
+const { getState } = require('./state');
+
+function setupSocket(io) {
+  io.on('connection', function(socket) {
+    var s = getState();
+    socket.emit('init', {
+      connections: s.connections, agentStatus: s.agentStatus,
+      systemInfo: s.systemInfo, sshdStatus: s.sshdStatus,
+      remoteStatus: s.remoteStatus,
+      metrics: s.metrics, alerts: s.alerts
+    });
+
+    socket.on('disconnect', function() {});
+  });
+}
+
+module.exports = { setupSocket };
+
